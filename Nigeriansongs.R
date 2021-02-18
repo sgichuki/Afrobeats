ec926a3ffc0b16499f2817333529ebad33fbcb56
sgichuki <53834942+sgichuki@users.noreply.github.com>
2021-02-18 18:00:36 +0100

    Top-artist-summary
    Filter out top artists based on the mean of their songs' popularity in this data set

--- Nigeriansongs.R
+++ Nigeriansongs.R
@@ -16,7 +16,7 @@ nigeriansongs$artist_top_genre<-as.factor(nigeriansongs$artist_top_genre)
 nigeriansongs$release_date<-as.factor(nigeriansongs$release_date)
 
 #most commonly occuring music genre in Afrobeat music
-ggplot(nigeriansongs, aes(artist_top_genre)) +
+ggplot(nigeriansongs, aes(artist_top_genre))+
   geom_bar(fill = "dodgerblue")+coord_flip()+
   theme_bw()+xlab("music genres")
 
@@ -51,7 +51,7 @@ set2019 <- set2019[-c(11),]
 
 set2020 <-filter(nigeriansongs,popularity>50,release_date==2020)
 
-songplot1=ggplot(data = set2017, mapping = aes(x = reorder(name, popularity),popularity)) + 
+songplot1=ggplot(data = set2017, mapping = aes(x = reorder(name, popularity),popularity))+ 
   geom_bar(stat = "Identity",fill="goldenrod")+ 
   ggtitle("Most popular songs 2017")+
   coord_flip()+xlab("")+theme_bw()+
@@ -122,10 +122,11 @@ davidograph=ggplot(data = Davido_df, mapping = aes(x = reorder(name, danceabilit
   geom_bar(stat = "identity",position = "dodge",alpha=0.5,fill="lightgoldenrod1",colour="black")+ 
   coord_flip()+theme(axis.text = element_text(size = 10))+
   scale_x_discrete(labels=wrap_format(10))
+
 ggsave("davidograph.jpg",width = 297,height = 210,units = c("mm"),dpi = 300)
 
 # What are the most danceable songs
-danceabilityplot=ggplot(data = popularsongs, mapping = aes(x = reorder(name, danceability), danceability))+ 
+danceabilityplot=ggplot(data = popularsongs,mapping = aes(x = reorder(name, danceability),danceability))+ 
    geom_bar(stat = "identity",fill="blanchedalmond",color="orangered")+
    xlab("")+coord_flip()+ theme_bw()+
   ggtitle("Most danceable Afrobeats on Spotify 1998-2020")
@@ -134,15 +135,21 @@ danceabilityplot=ggplot(data = popularsongs, mapping = aes(x = reorder(name, dan
 ggsave("danceabilityplot.pdf",width = 297,height = 210,units = c("mm"),dpi = 300)
 ggsave("danceabilityplot.jpg",width = 297,height = 210,units = c("mm"),dpi = 300)
 
+#Artists with highest average popularity scores
+artist_summary <- nigeriansongs %>%
+  group_by(artist) %>%
+  summarise(popularity = mean(popularity, na.rm = TRUE))
+
 #Highest popularity score is 73, filter out those with 40 and above
-topartist<-filter(artist_summary, popularity > 39)
+topartist<-filter(artist_summary,popularity > 39)
 
 #Top African artists on Spotify
-ggplot(data = topartist, mapping = aes(x = reorder(artist, popularity), popularity)) + 
+topartist=ggplot(data = topartist, mapping = aes(x = reorder(artist, popularity), popularity))+ 
   geom_bar(stat = "Identity",fill="blanchedalmond",color="orangered3") + 
-  ggtitle("Most popular African artists Spotify")+
+  ggtitle("Most popular African artists Spotify 1998-2020")+
   coord_flip()+ theme_bw()+
   scale_x_discrete(labels(wrap_format(10))+
                      theme_light())
 
-
+#Save plots as either pdf or jped                   
+ggsave("topartist.jpg",width = 297,height = 210,units = c("mm"),dpi = 300)
